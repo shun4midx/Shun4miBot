7fa7a2fd61da4238c2645791364e56f1bb5aafaa
shun4midx@gmail.com <shun4midx@gmail.com>
2025-02-04 06:41:14 +0800

    Joke Zhuyin function

---
+++ src/zhuyin/zhuyin.h
@@ -0,0 +1,14 @@
+/********************************************
+ * Copyright (c) 2025 Shun/翔海 (@shun4midx) *
+ * Project: Shun4miBot                      *
+ * File Type: Header file                   *
+ * File: zhuyin.h                           *
+ ********************************************/
+
+#pragma once
+
+#include <iostream>
+
+std::vector<std::string> zhuyinify(std::string str); // Converts str into Zhuyin and if there is any edits required, it's appended to the back of the vector
+std::string zhuyinifyString(std::string str); // Returns what the str is converted into Zhuyin
+std::string zhuyinDict(); // Returns the entire txt file of the current custom Zhuyin dictionary available entries
